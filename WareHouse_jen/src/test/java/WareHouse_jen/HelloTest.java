d283e925ddba32ad5c1fa96a56b8f2aa12946885
AmitMaheta <AmitMaheta@LAPTOP-FA9S20QQ>
2018-08-14 14:07:37 +0100

    Failed Test

---
+++ WareHouse_jen/src/test/java/WareHouse_jen/HelloTest.java
@@ -0,0 +1,20 @@
+package WareHouse_jen;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	 
+@Test
+    public void testAdd() {
+       String str= "Junit is working fine";
+       assertEquals("Junit is working fine",str);
+    }
+}
